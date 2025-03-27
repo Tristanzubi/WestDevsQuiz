2aae6ccf04e7fb0b99300172f2f79765b498a915
Estelle Michard <michardestelleem@gmail.com>
2025-03-27 10:15:24 +0100

    Sixième enregistrement page Result HTML CSS OK JS en cours

--- script/result.js
+++ script/result.js
@@ -1,14 +1,29 @@
 const resultScore = document.querySelector("#result-score");
 const resultImage = document.querySelector("#result-image");
-
-let score = 0;
-
-if (result >= 4) {
-    resultImage.src = '../Assets/Logo_WestDevQuiz.png';
-} else if (result >= 2) {
-    resultImage.src = '../Assets/Logo_WestDevQuizBof.png';
-} else {
-    resultImage.src = '../Assets/Logo_WestDevQuizBad.png';
+const reponseUtilisateur = localStorage.getItem("q" + i);
+const reponseCorrect {
+    q1 = "",
+    q2 = "",
+    q3 = "",
+    q4 = "",
+    q5 = "",
 }
 
-console.log(score === 4);
+let score = 0;
+for (let i = 0; i <= 5; i++) {
+} if (reponseUtilisateur === reponseCorrect["q" + i]) {
+    score++;
+}
+
+document.querySelector("#resultScore").innerText = "Votre score est de ${ score }/5 !";
+
+if (score >= 4) {
+    resultImage.src = '../Assets/Logo_WestDevQuiz.png';
+    resultScore.innerText = 'Bravo !';
+} else if (score >= 2) {
+    resultImage.src = '../Assets/Logo_WestDevQuizBof.png';
+    resultScore.innerText = 'Pas mal du tout !';
+} else {
+    resultImage.src = '../Assets/Logo_WestDevQuizBad.png';
+    resultScore.innerText = 'Et si on révisait pour réessayer ?';
+}
