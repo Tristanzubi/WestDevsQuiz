c0a345e24b43018e4be019060b4ab840142b8f1b
B-Maxp <max.bpr@outlook.fr>
2025-03-28 10:41:16 +0100

    :wrench:Update timer add question, score and functional robot

--- script/question.js
+++ script/question.js
@@ -1,19 +1,31 @@
-let secondes = 10;
-const timer = document.getElementById("timeout");
-const chrono = window.setInterval(tictac, 1000)
-
-function tictac() {
-    secondes--;
-    if (secondes === 0 || !timer) {
-        window.clearTimeout(chrono);
-        return;
-    }
-    timer.innerHTML = secondes
-}
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    let secondes = 10;
+    const timer = document.getElementById("timeout");
+    const reponses = document.querySelectorAll(".reponse");
+    const chrono = window.setInterval(tictac, 1000)
+
+    function tictac() {
+        secondes--;
+        if (secondes === -1 || !timer) {
+            window.clearTimeout(chrono);
+            rightAnwser()
+            return;
+        }
+        timer.innerHTML = secondes
+    }
+
+    function rightAnwser() {
+        reponses.forEach(button => {
+            button.disabled = true;
+            button.style.opacity = "0.5";
+
+            if (button.getAttribute("value") === "true") {
+                button.style.backgroundColor = "rgb(112, 213, 236)";
+                button.style.opacity = "1";
+            }
+        });
+    }
+
 
     let score;
     if (localStorage.getItem("result")) {
@@ -22,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
         score = 0;
     }
 
-    const reponse = document.querySelectorAll(".reponse")
+    const reponse = document.querySelectorAll(".reponse");
 
     reponse.forEach(button => {
         button.addEventListener("click", () => {
@@ -50,15 +62,25 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
     console.log(score);
+
+
+
+    let finalScore = localStorage.getItem("result") || 0;
+    document.querySelector("#resultScore").innerHTML = `<p>Votre score est de <br> ${finalScore} /10</p> `;
+    function resetScore() {
+        localStorage.removeItem("#score")
+    }
+    function resetScore() {
+        localStorage.removeItem("#score")
+    }
+
+    let robot = document.querySelector("main section img");
+
+    if (finalScore >= 8) {
+        robot.src = '../Assets/Logo_WestDevQuiz.png';
+    } else if (finalScore >= 5) {
+        robot.src = '../Assets/Logo_WestDevQuizBof.png';
+    } else {
+        robot.src = '../Assets/Logo_WestDevQuizBad.png';
+    }
 });
-
-
-
-const score = localStorage.getItem("result") || 0;
-document.querySelector("#resultScore").innerHTML = `<p>votre score est de <br> ${score} /5</p> `;
-function resetScore() {
-    localStorage.removeItem("#score")
-}
-function resetScore() {
-    localStorage.removeItem("#score")
-}
